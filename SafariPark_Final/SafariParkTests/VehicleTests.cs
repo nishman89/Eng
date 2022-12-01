c30aae4e4c075f167e1d3e9d9a1f302818cebd3f
nishman89 <53156243+nishman89@users.noreply.github.com>
2022-12-01 18:29:32 +0000

    Added failing test

--- SafariPark_Final/SafariParkTests/VehicleTests.cs
+++ SafariPark_Final/SafariParkTests/VehicleTests.cs
@@ -11,6 +11,11 @@ namespace SafariParkTests
     public class VehicleTests
     {
         [Test]
+        public void FailingTest()
+        {
+            Assert.Fail();
+        }
+        [Test]
         public void WhenADefaultVehicleIsConstructedItsPositionIsZero()
         {
             Vehicle v = new Vehicle();
